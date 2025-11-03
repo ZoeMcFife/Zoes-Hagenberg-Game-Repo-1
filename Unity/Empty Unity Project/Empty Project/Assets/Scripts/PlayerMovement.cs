977979435c53eb161aa868bb243c0cbfef4cc4ae
Zoe McFife <cmdrprotogen27@gmail.com>
2025-11-03 17:57:55 +0100

    Add camera and player movement scripts
    Introduced CameraSlideMovement script for basic forward camera movement and logging. Refactored PlayerMovement to use CharacterController and InputSystem for movement and gravity, and updated scene to add new Player GameObject and adjust transforms.

--- Unity/Empty Unity Project/Empty Project/Assets/Scripts/PlayerMovement.cs
+++ Unity/Empty Unity Project/Empty Project/Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,47 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float moveSpeed = 5f;
+
+    private float gravity = -9.81f;
+    private Vector3 velocity;
+    
+    private CharacterController controller;
+    private InputAction moveAction;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
+        moveAction = InputSystem.actions.FindAction("Move");
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Moew");
+        Move();
+        ApplyGravity();
+    }
+
+    private void ApplyGravity()
+    {
+        if (controller.isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+            return;
+        }
         
-        transform.Translate(Vector3.forward * Time.deltaTime);
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+    
+    private void Move()
+    {
+        Vector2 moveInput  = moveAction.ReadValue<Vector2>();
+        Vector3 moveDirection = new Vector3(moveInput.x, 0, moveInput.y);
+        moveDirection = transform.TransformDirection(moveDirection);
+        controller.Move(moveDirection * (moveSpeed * Time.deltaTime));
     }
 }
